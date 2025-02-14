0e4d2e2cef9ccb8693a136ee5981854edc511bd7
gaurangsaini01 <gaurangsaini01@gmail.com>
2025-02-14 21:48:29 +0530

    Refactor OTP model and clean up Template component

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const {mailSender} = require("../utils/mailsender")
-const otpTemplate = require("../mail/templates/emailVerification")
+const { mailSender } = require("../utils/mailsender");
+const otpTemplate = require("../mail/templates/emailVerification");
 const otpSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-  }, 
+  },
   otp: {
     type: String,
     required: true,
@@ -20,18 +20,14 @@ const otpSchema = new mongoose.Schema({
 
 otpSchema.pre("save", async function (next) {
   try {
-    ("I am in pre save hook")
+    console.log(this.otp);
     const mailResponse = await mailSender(
       this.email,
       "Verification E-mail from StudyNotion",
       otpTemplate(this.otp)
     );
-    ('Email sent successfully',mailResponse)
     next();
-  } catch (err) {
-    (`Error in sending email`,err);
-  }
+  } catch (err) {}
 });
 
 module.exports = mongoose.model("OTP", otpSchema);
-
